3f97a2c352a7332b51277793d65afe72ac5fe9ec
Florian Chaudret <florian.chaudret@gmail.com>
2019-05-19 00:37:29 +0200

    Add test counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="counter">
+        <h1>my counter</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component
+export default class Counter extends Vue {
+    mounted: {
+        console.log('mounted')
+    }
+}
+
+
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <Counter></Counter>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
@@ -8,10 +9,12 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Counter  from '@/components/Counter'
 
 @Component({
   components: {
     HelloWorld,
+    Counter,
   },
 })
 export default class Home extends Vue {}
